38558e341bd0b58f28b4b0ef4eb0b3d00ee99b2b
Shivaraj Chougala <shivarajkchougala2020@gmail.com>
2025-09-25 00:35:56 +0530

    new changes

--- First.js
+++ First.js
@@ -1,1 +1,13 @@
 console.log("hello world")
+
+let name="shivaraj"
+console.log(name);
+
+let number=new Number(10);
+console.log(number);
+console.log(number-100);
+
+let date=new Date();
+console.log(date.toLocaleDateString());
+
+

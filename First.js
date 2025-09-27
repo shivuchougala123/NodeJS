02acd8f46e2a9379be52eb6f1d4b9214bb55e137
Shivaraj Chougala <shivarajkchougala2020@gmail.com>
2025-09-27 23:00:03 +0530

    till objects

--- First.js
+++ First.js
@@ -1,13 +1,46 @@
-console.log("hello world")
+// console.log("hello world")
 
-let name="shivaraj"
-console.log(name);
+let number=10;
+let str="shiv";
+let bank=100.22;
+let bool=true;
+let best=120390120n;
+let Nl=null;
+let unified;
 
-let number=new Number(10);
-console.log(number);
-console.log(number-100);
+// console.log(typeof number);
+// console.log(typeof str);
+// console.log(typeof bank);
+// console.log(typeof bool);
+// console.log(typeof best);
+// console.log(typeof Nl);
+// console.log(typeof unified);
+// let id=Symbol("10")
+// let id1=Symbol("20")
+// console.log(id1==id);
 
-let date=new Date();
-console.log(date.toLocaleDateString());
+// console.log(+"");
+
+ console.log(null==0)     
+                console.log(null>=0)    
+                console.log(null>0)      
+
+// let age=12
+// let a=Symbol("id")
+
+// let obj={
+
+// name:"shiv",
+// [a]:-1
+//             }
+
+// console.log(age);
+// console.log(obj[a]);
 
 
+// console.log(obj);
+// console.log(obj.name);
+
+
+const bigNumber=3094829n
+console.log(typeof bigNumber);
